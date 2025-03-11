fffc9c8db94fc04a40a161ad4ee58f0aea14f495
李英杰 <18281539205@163.com>
2025-03-11 22:27:56 +0800

    husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import prettierConfig from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier/recommended";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}", ".prettierrc.js"],
-    env: {
-      node: true, // 启用 Node.js 全局变量（如 module、require）‌:ml-citation{ref="1,5" data="citationList"}
-    },
-    rules: {
-      "no-undef": "off", // 关闭未定义变量检查‌:ml-citation{ref="1,5" data="citationList"}
-    },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}', '.prettierrc.js'],
   },
   {
     languageOptions: {
@@ -27,10 +21,10 @@ export default [
   {
     rules: {
       // 显式启用与 Prettier 风格一致的 ESLint 规则
-      quotes: ["error", "single"], // 强制单引号 ‌:ml-citation{ref="3" data="citationList"}
-      semi: ["error", "never"], // 禁用分号 ‌:ml-citation{ref="3" data="citationList"}
+      //quotes: ['error', 'single'], // 强制单引号 ‌:ml-citation{ref="3" data="citationList"}
+      //semi: ['error', 'never'], // 禁用分号 ‌:ml-citation{ref="3" data="citationList"}
     },
   },
   prettierPlugin, // 应用 Prettier 格式化规则
   prettierConfig,
-];
+]
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const aa = "aa";
+  //const aa = 'aa'
   return (
     <div className="App">
       <header className="App-header">
